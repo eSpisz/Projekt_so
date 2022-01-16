252f3f3925dfeebcd9c3dc423a2a541ba65b4d8b
eSpisz <83001083+eSpisz@users.noreply.github.com>
2022-01-16 13:18:38 +0100

    2 wersja
    dołożony Round robin

--- Round_robin.py
+++ Round_robin.py
@@ -1,15 +1,30 @@
 kolejka = []  # PID
-czas_dla_procesu=int(input("proszę podać czas dla każdego procesu (>1): ")) #interwał czasowy jaki zostanie przydzielony procesowi na wykonanie
-licznik_pomocniczy=czas_dla_procesu
-def round_robin(czas, procesy):
-    for key, czas_przyjscia in procesy.items():
+licznik_pomocniczy = 0  #zmienna, która wskazuje kiedy powinno nastąpić przesunięcie kolejki
+czas_dla_procesu = 0  #stała która służy do resetowania zmiennej licznik_pomocniczy
+
+def przypisanie_wartosci():                         #funkcja wywolywana w main.py aby przekazac wartosc interwału czasu 
+    global licznik_pomocniczy
+    global czas_dla_procesu
+    licznik_pomocniczy = int(input("proszę wybrać interwał czasowy dla każdego procesu: "))
+    czas_dla_procesu = licznik_pomocniczy
+
+
+def round_robin(czas, procesy):         #ciało funkcji tworzacej kolejke dla procesora 
+    global licznik_pomocniczy
+    global czas_dla_procesu
+    for key, czas_przyjscia in procesy.items():    #pętla dodająca proces, którzy nadszedł do kolejki 
         if czas_przyjscia[0] == czas:
             kolejka.append(key)
-            licznik_pomocniczy=czas_dla_procesu
-    if licznik_pomocniczy ==0:
-        zmienna_pomocnicza=kolejka[0]
-        kolejka.pop(0)
-        kolejka.append(zmienna_pomocnicza)
+    if licznik_pomocniczy ==0:          
+        try:
+            zmienna_pomocnicza=kolejka[0]    #blok kodu odpowiedzialny za przesuwanie kolejki
+        except:
+            zmienna_pomocnicza=0
+        else:
+            kolejka.pop(0)
+            kolejka.append(zmienna_pomocnicza)
+            licznik_pomocniczy += czas_dla_procesu
     if licznik_pomocniczy>0:
         licznik_pomocniczy-=1
     return kolejka
+
--- main.py
+++ main.py
@@ -3,19 +3,21 @@ import Round_robin
 
 
 stat_procesy={1:[0,0],2: [0,0], 3: [0, 0]} #statystyka procesow, key=PID, wartosc tablicy[0]=czas zakończenia, wartosc tablicy[1]=czas oczekiwania
-procesy = {1: [0, 1], 2: [1, 2], 3: [3, 3]}
+procesy = {1: [0, 5], 2: [1, 5], 3: [2, 5]}
 czas=0
-#wybor_algorytmu_szeregowania=input("proszę wybrać algorytm szeregowania (wpisać odpowiadającą liczbę): 1.FCFS 2.Round-Robin")   #wybor algorytmu
+wybor_algorytmu_szeregowania=int(input("proszę wybrać algorytm szeregowania (wpisać odpowiadającą liczbę) 1.FCFS 2.Round-Robin: "))   #wybor algorytmu
+if wybor_algorytmu_szeregowania == 2:
+    Round_robin.przypisanie_wartosci()
+
+
 while True:
-    kolejka = Round_robin.round_robin(czas, procesy)
-    print(kolejka)
-    #if wybor_algorytmu_szeregowania==int(1):                        #obsługa błedu wyboru
-        #kolejka=FCFS.fcfs(czas, procesy)
-    #elif wybor_algorytmu_szeregowania==int(2):
-        #kolejka = Round_robin.round_robin(czas, procesy)
-    #else:
-        #wybor_algorytmu_szeregowania=input("Niepoprawny wybór, proszę spróbować jeszcze raz:")
-    if czas == 20:                                 #warunek kończący po upływie określonego czasu
+    if wybor_algorytmu_szeregowania==1:                        #obsługa błedu wyboru
+        kolejka=FCFS.fcfs(czas, procesy)
+    elif wybor_algorytmu_szeregowania==2:
+        kolejka = Round_robin.round_robin(czas, procesy)
+    else:
+        wybor_algorytmu_szeregowania=input("Niepoprawny wybór, proszę spróbować jeszcze raz:")
+    if czas == 30:                                 #warunek kończący po upływie określonego czasu
         break
     try:
         if procesy[kolejka[0]][1]>0:        #wykonywanie procesu
@@ -30,7 +32,7 @@ while True:
         czas+=1
         continue
     czas += 1  # licznik czasu
-    
+    print(kolejka)
         
 print(procesy)
 print(stat_procesy)
